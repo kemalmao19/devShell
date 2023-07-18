2ab0922e485c2bd614048dd146661b6878d703f7
kemalmao19 <sebelumtidur14@gmail.com>
2023-07-19 00:37:18 +0700

    first

---
+++ python/flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "This is my python dev";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
+  };
+
+  # Flake outputs
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    # Systems supported
+    allSystems = [
+      "x86_64-linux" # 64-bit Intel/AMD Linux
+      # "aarch64-linux" # 64-bit ARM Linux
+      # "x86_64-darwin" # 64-bit Intel macOS
+      # "aarch64-darwin" # 64-bit ARM macOS
+    ];
+
+    # Helper to provide system-specific attributes
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    # Development environment output
+    devShells = forAllSystems ({pkgs}: {
+      default = let
+        # Use Python 3.11
+        python = pkgs.python311;
+      in
+        pkgs.mkShell {
+          # The Nix packages provided in the environment
+          packages = [
+            # Python plus helper tools
+            (python.withPackages (ps:
+              with ps; [
+                pandas
+                numpy
+              ]))
+          ];
+        };
+    });
+  };
+} 
