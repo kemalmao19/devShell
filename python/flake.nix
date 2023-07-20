c038eaaf61faef30f9f4ba4bda4643b00cfb6684
kemalmao19 <sebelumtidur14@gmail.com>
2023-07-21 02:26:42 +0700

    formatting

--- python/flake.nix
+++ python/flake.nix
@@ -7,43 +7,38 @@
   };
 
   # Flake outputs
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    # Systems supported
-    allSystems = [
-      "x86_64-linux" # 64-bit Intel/AMD Linux
-      # "aarch64-linux" # 64-bit ARM Linux
-      # "x86_64-darwin" # 64-bit Intel macOS
-      # "aarch64-darwin" # 64-bit ARM macOS
-    ];
+  outputs = { self, nixpkgs, }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
 
-    # Helper to provide system-specific attributes
-    forAllSystems = f:
-      nixpkgs.lib.genAttrs allSystems (system:
-        f {
-          pkgs = import nixpkgs {inherit system;};
-        });
-  in {
-    # Development environment output
-    devShells = forAllSystems ({pkgs}: {
-      default = let
-        # Use Python 3.11
-        python = pkgs.python311;
-      in
-        pkgs.mkShell {
+      # Helper to provide system-specific attributes
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs allSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default = let
+          # Use Python 3.11
+          python = pkgs.python311;
+        in pkgs.mkShell {
           # The Nix packages provided in the environment
           packages = [
             # Python plus helper tools
             (python.withPackages (ps:
               with ps; [
-              
+
                 #scientific computing
                 pandas # Data structures & tools
                 numpy # Array & matrices
                 scipy # Integral, solving differential, equations, optimizations)
-                
+
                 # Visualization
                 matplotlib # plot & graphs
                 seaborn # heat maps, time series, violin plot
@@ -51,11 +46,10 @@
                 # Algorithmic Libraries
                 scikit-learn-extra # Machine learning: regression, classificatons,..
                 statsmodels # Ecplore data, estimate statistical models, & perform statistical test.
-                
-                                
+
               ]))
           ];
         };
-    });
-  };
-} 
+      });
+    };
+}
