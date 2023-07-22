6d17f14329732c0ccdb8695c8a9343265e8c986f
kemalmao19 <sebelumtidur14@gmail.com>
2023-07-22 18:33:41 +0700

    add python310

---
+++ python/python311/README.md
@@ -0,0 +1,5 @@
+# Features
+- Python 310
+- Jupyter Notebook
+- Basic Data Science Libraries
+- You can add more libraries, just go here https://search.nixos.org/packages
---
+++ python/python311/flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "This is my python dev: Data Science Libraries Edition";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs, }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs allSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default = 
+        let
+          # Use Python 3.11
+          python = pkgs.python310;
+          
+        in pkgs.mkShell {
+          # The Nix packages provided in the environment
+          packages = [
+            # Python plus helper tools
+            (python.withPackages (ps:
+              with ps; [
+
+                # notebook
+                jupyter
+                jupyterlab
+                ipykernel
+                ipython
+
+                # scientific computing
+                pandas # Data structures & tools
+                numpy # Array & matrices
+                scipy # Integral, solving differential, equations, optimizations)
+
+                # Visualization
+                matplotlib # plot & graphs
+                seaborn # heat maps, time series, violin plot
+
+                # Algorithmic Libraries
+                scikit-learn-extra # Machine learning: regression, classificatons,..
+                statsmodels # Ecplore data, estimate statistical models, & perform statistical test.
+
+                # Formatting
+                black
+
+              ]))
+          ];
+        };
+      });
+    };
+}
