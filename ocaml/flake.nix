bf88b7ef7e3d283245dcb25cf79e7b7e07b8fc8b
kemalmao19 <sebelumtidur14@gmail.com>
2023-07-22 19:13:08 +0700

    add ocaml

--- ocaml/flake.nix
+++ ocaml/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "This is my python dev: Data Science Libraries Edition";
+  description = "Ocaml Environment";
 
   # Flake inputs
   inputs = {
@@ -7,7 +7,7 @@
   };
 
   # Flake outputs
-  outputs = { self, nixpkgs, }:
+  outputs = { self, nixpkgs }:
     let
       # Systems supported
       allSystems = [
@@ -18,31 +18,20 @@
       ];
 
       # Helper to provide system-specific attributes
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs allSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
-    in {
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
       # Development environment output
       devShells = forAllSystems ({ pkgs }: {
-        default = 
-        let
-          # Use Python 3.11
-          ocaml = pkgs.ocaml-ng.ocamlPackages_4_14;
-          
-        in pkgs.mkShell {
+        default = pkgs.mkShell {
           # The Nix packages provided in the environment
-          packages = [
-            # Python plus helper tools
-            (ocaml.withPackages (ps:
-              with ps; [
-                opam
-                pkg-config
-                libev
-                oniguruma
-                openssl
-                gmp
-              ]))
-          ];
+          packages = with pkgs; [
+            ocaml
+            opam
+            # merlin
+           ];
         };
       });
     };
